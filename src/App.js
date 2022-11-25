4705caf8b442bd83e24480f9b07f4f848f6f3d10
Victor Dracxler <victordracxler@poli.ufrj.br>
2022-11-25 19:51:41 -0300

    feat: product page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import GlobalStyle from "./assets/styles/GlobalStyle";
-import CartPage from "./pages/CartPage/CartPage";
-import InitialPage from "./pages/InitialPage/InitialPage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import SignUpPage from "./pages/SignUpPage/SignUpPage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import GlobalStyle from './assets/styles/GlobalStyle';
+import CartPage from './pages/CartPage/CartPage';
+import InitialPage from './pages/InitialPage/InitialPage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import ProductPage from './pages/ProductPage/ProductPage';
+import SignUpPage from './pages/SignUpPage/SignUpPage';
 
 function App() {
-  return (
-    <>
-    <GlobalStyle/>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<InitialPage/>}/>
-        <Route path="/entrar" element={<LoginPage/>}/>
-        <Route path="/cadastro" element={<SignUpPage/>}/>
-        <Route path="/carrinho" element={<CartPage/>}/>
-      </Routes>
-    </BrowserRouter>
-    </>
-  );
+	return (
+		<>
+			<GlobalStyle />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<InitialPage />} />
+					<Route path="/entrar" element={<LoginPage />} />
+					<Route path="/cadastro" element={<SignUpPage />} />
+					<Route path="/carrinho" element={<CartPage />} />
+					<Route path="/produto/:id" element={<ProductPage />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
--- src/components/ProductThumbnail.js
+++ src/components/ProductThumbnail.js
@@ -1,26 +1,50 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export default function ProductThumbnail(product) {
 	const { _id, name, type, price, image } = product;
 
 	return (
-		<ProductContainer key={_id}>
-			<img src={image} alt={name} />
-			<h1>{name}</h1>
-			<h2>{price}</h2>
-		</ProductContainer>
+		<StyledLink key={_id} to={`/produto/${_id}`}>
+			<ProductContainer>
+				<img src={image} alt={name} />
+				<div>
+					<h1>{name}</h1>
+					<h2>R$ {price.toFixed(2)}</h2>
+				</div>
+			</ProductContainer>
+		</StyledLink>
 	);
 }
 
+const StyledLink = styled(Link)`
+	text-decoration: none;
+`;
+
 const ProductContainer = styled.div`
-	height: 209px;
+	height: 250px;
 	width: 145px;
 	border-radius: 3px;
 	padding: 8px;
 	box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.1);
 	margin: 0 15px 11px 15px;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	justify-content: space-between;
+	color: #000000;
 
 	img {
 		width: 100%;
+		max-height: 200px;
+	}
+
+	h1 {
+		font-weight: 700;
+		font-size: 18px;
+	}
+	h2 {
+		font-size: 16px;
+		font-weight: 700;
 	}
 `;
